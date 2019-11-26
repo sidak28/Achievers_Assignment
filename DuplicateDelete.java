a9011b9dc92ba6082f387b9d41524af0dc3642ec
sidak28 <44256922+sidak28@users.noreply.github.com>
2019-11-26 17:55:54 +0530

    Add files via upload

---
+++ DuplicateDelete.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class DuplicateDelete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the String");
+		String str=new String();
+		str=sc.next();
+		int n=str.length();
+		String str1="";
+		for(int i=0;i<n-1;i++)
+		{
+			if(i==0)
+			{
+			if(str.charAt(i)!=str.charAt(i+1))
+				{
+					str1+=str.charAt(i);
+				}
+			}
+			else if(i>0)
+			{
+				if(str.charAt(i)!=str.charAt(i+1)&&str.charAt(i)!=str.charAt(i-1))
+				{
+					str1+=str.charAt(i);
+				}	
+			}
+		}
+		if(str.charAt(n-1)!=str.charAt(n-2))
+		{
+			str1+=str.charAt(n-1);
+		}
+		System.out.println("The string after deleting adjacent duplicates is: ");
+		System.out.print(str1);
+	}
+}
